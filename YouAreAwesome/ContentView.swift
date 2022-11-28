b47bd83cc8b6de43a10d5c3828a0e5fa690f4991
James Bell <james@belldesign.com.au>
2022-11-28 20:02:25 +1100

    ch 1.6 complete

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -8,36 +8,85 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var myString = "when you dont know who to call call those bastardes"
+    @State private var filled = 1.0
+    
+    @State private var bgColor = Color("SkyBlue")
+    
     var body: some View {
-        VStack {
-            HStack {
-                Image(systemName: "snowflake")
-                    .resizable()
-                    .scaledToFit()
-                    .imageScale(.large)
-                    .foregroundColor(.red)
-                    .padding(10)
+        ZStack {
+            Color.cyan
+                .ignoresSafeArea()
+            
+            VStack {
+                Text("padding ")
+                    .font(.largeTitle)
+                    .fontWeight(.black)
+                    .foregroundColor(.gray)
+                    .opacity(0.7)
+                    .background {
+                        Image(systemName: "swift")
+                            .resizable()
+                            .foregroundColor(.purple)
+                            .scaledToFit()
+                    }
+                    .background(){
+                        bgColor
+    //                    Color("BostonCollegeGold")
+                        .opacity(0.5)
+                    }
+                    .cornerRadius(15)
                     
-                Image(systemName: "flame")
-                    .resizable()
-                    .scaledToFit()
-                    .imageScale(.large)
-                    .foregroundColor(.red)
-                    .padding(20)
-
-                .background(.blue)
+                
+                Spacer()
+                Text(myString)
+                    .font(.largeTitle)
+                    .fontWeight(/*@START_MENU_TOKEN@*/.heavy/*@END_MENU_TOKEN@*/)
+                    .minimumScaleFactor(0.5)
+                    .foregroundColor(Color.orange)
+                    .multilineTextAlignment(.trailing)
+                    .italic()
+                    .kerning(2.3)
+    //                .frame(width: .infinity, height: 150)
+                    .frame(maxWidth: .infinity)
+                    .background(.black)
+                    .border(.yellow, width: 3)
+                    .padding(14)
+                
+                Spacer()
+                
+                Gauge(value: filled, in: 0...10 ) {
+                    Text("Fill me up")
+                }.gaugeStyle(.accessoryCircular)
+                .padding()
+                
+                HStack(spacing: 10){
+                    Button("Fab"){
+                        myString = "Fabulous thats you"
+                        filled += 1
+                        bgColor = Color.purple
+                    }
+                    .buttonStyle(.borderedProminent)
+                    
+                    Button("Great"){
+                        myString = "You are Great!"
+                        filled = filled-1
+                    }
+                    .buttonStyle(.borderedProminent)
+                    
+                    Button("another button") {
+                        myString = ""
+                        filled = 5
+                    }.buttonStyle(.borderedProminent)
+                        .buttonBorderShape(.roundedRectangle(radius: 4))
+                }.border(.purple, width: 5)
+                    .padding()
+                    .border(.blue)
+                
+                    .padding()
             }
-            Text("You are awesome!")
-                .font(.largeTitle)
-                .fontWeight(/*@START_MENU_TOKEN@*/.heavy/*@END_MENU_TOKEN@*/)
-                .foregroundColor(Color.orange)
-                .italic()
-                .padding(14)
-                .kerning(2.3)
-                .background(.black)
         }
-        .padding()
-        .background(.yellow)
     }
 }
 
