bc4b08803130b0dd67d6728306fd6a94eace3cc3
James Bell <james@belldesign.com.au>
2022-11-30 22:26:46 +1100

    ch 1.10 complete var person = "Limor"
    switch person {
    case "Limor":
        print("hello Lady Ada")
    case "Grace":
        print("hello Admiral Hopper")
    case "Beyoncé":
        print("All hail Queen B")
    default:
        print("Hello there, \(person)")
    }
    
    //Ternary Operator
    
    let result = (person == "Limor" ? "Hello Lady Ada" : "Hello There \(person)" )
    print(result)
    
    print(person == "Limor" ? "Hello Lady Ada" : "Hello there \(person)")

---
+++ Conditionals.playground/Contents.swift
@@ -0,0 +1,39 @@
+import UIKit
+
+
+//var messageString = "I'm a developer!"
+//
+//print(messageString)
+//print("Concatination is wired"+messageString)
+//print("Concatination is wired, \(messageString)", "promise")
+//
+//var person = "Prot. G."
+//if person == "Limor!" {
+//    print ("Hello, Lady Ada!")
+//} else if person == "Grace" {
+//    print ("Hello, Admiral Hopper!")
+//} else {
+//    print ("Hello There!")
+//}
+
+
+var person = "Limor"
+
+switch person {
+case "Limor":
+    print("hello Lady Ada")
+case "Grace":
+    print("hello Admiral Hopper")
+case "Beyoncé":
+    print("All hail Queen B")
+default:
+    print("Hello there, \(person)")
+}
+
+
+//Ternary Operator
+
+let result = (person == "Limor" ? "Hello Lady Ada" : "Hello There \(person)" )
+print(result)
+
+print(person == "Limor" ? "Hello Lady Ada" : "Hello there \(person)")
--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -9,82 +9,63 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State private var myString = "when you dont know who to call call those bastardes"
+    @State private var myString = "namaste"
     @State private var filled = 1.0
+    @State private var up = true
     
     @State private var bgColor = Color("SkyBlue")
     
     var body: some View {
         ZStack {
-            Color.cyan
+            Rectangle()
+                .fill(
+                    AngularGradient(colors: [ .red, .orange, .yellow, .green, .blue, .indigo, .purple],
+                                    center: UnitPoint(x: 0.3, y: 0.7),
+                                    angle: Angle(degrees:100))
+                )
                 .ignoresSafeArea()
             
             VStack {
-                Text("padding ")
-                    .font(.largeTitle)
-                    .fontWeight(.black)
-                    .foregroundColor(.gray)
-                    .opacity(0.7)
-                    .background {
-                        Image(systemName: "swift")
-                            .resizable()
-                            .foregroundColor(.purple)
-                            .scaledToFit()
-                    }
-                    .background(){
-                        bgColor
-    //                    Color("BostonCollegeGold")
-                        .opacity(0.5)
-                    }
-                    .cornerRadius(15)
-                    
-                
                 Spacer()
-                Text(myString)
-                    .font(.largeTitle)
-                    .fontWeight(/*@START_MENU_TOKEN@*/.heavy/*@END_MENU_TOKEN@*/)
-                    .minimumScaleFactor(0.5)
-                    .foregroundColor(Color.orange)
-                    .multilineTextAlignment(.trailing)
-                    .italic()
-                    .kerning(2.3)
-    //                .frame(width: .infinity, height: 150)
-                    .frame(maxWidth: .infinity)
-                    .background(.black)
-                    .border(.yellow, width: 3)
-                    .padding(14)
+                Image(systemName: "touchid", variableValue: filled)
+                    .resizable()
+                    .scaledToFit()
+                    .symbolRenderingMode(.multicolor)
+                //                    .background(Color.teal.opacity(0.5))
+                    .clipShape(Circle())
+                    .shadow(color: .gray, radius: 30, x: 10, y: 10)
+                //                    .foregroundColor(.pink)
+                    .padding(.bottom, 40)
+                    .padding(.horizontal,20)
                 
-                Spacer()
-                
-                Gauge(value: filled, in: 0...10 ) {
-                    Text("Fill me up")
-                }.gaugeStyle(.accessoryCircular)
+                Group{
+                    Text(myString)
+//                    Text(String(filled))
+                }
+                .font(.largeTitle)
+                .foregroundColor(.pink)
+                .fontWeight(.bold)
                 .padding()
                 
-                HStack(spacing: 10){
-                    Button("Fab"){
-                        myString = "Fabulous thats you"
-                        filled += 1
-                        bgColor = Color.purple
-                    }
-                    .buttonStyle(.borderedProminent)
-                    
-                    Button("Great"){
-                        myString = "You are Great!"
-                        filled = filled-1
-                    }
-                    .buttonStyle(.borderedProminent)
-                    
-                    Button("another button") {
-                        myString = ""
-                        filled = 5
-                    }.buttonStyle(.borderedProminent)
-                        .buttonBorderShape(.roundedRectangle(radius: 4))
-                }.border(.purple, width: 5)
-                    .padding()
-                    .border(.blue)
+                Spacer()
                 
-                    .padding()
+                Button("Show Message"){
+                    if up {
+                        filled = filled + 0.2
+                        if filled > 1 {
+                            up = false
+                        }
+                    } else {
+                        filled = filled - 0.2
+                        if filled < 0 {
+                            filled = 0
+                            up = true
+                        }
+                    }
+
+                    myString = (myString == "namaste" ? "I'm Britney, Bitch" : "namaste")
+                }
+                .buttonStyle(.borderedProminent)
             }
         }
     }
